228bf2fa9b6b171221672e4ee43ba72cdd419c42
adelaweidischova <48671822+adelaweidischova@users.noreply.github.com>
2022-01-13 16:43:04 +0000

    Deploying to docs from @ books-are-next/romeo-a-julie@7d139c9ca666c6447ce744d3ea17d4c13de63b52 🚀

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 /* global self, caches, fetch */
 
-const CACHE = 'cache-ce8edd7';
+const CACHE = 'cache-7d139c9';
 
 self.addEventListener('install', e => {
   e.waitUntil(precache()).then(() => self.skipWaiting());
@@ -40,7 +40,7 @@ self.addEventListener('activate', event => {
 });
 
 function precache() {
-  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./manifest.json","./index.html","./romeo_a_julie_001.html","./romeo_a_julie_002.html","./romeo_a_julie_003.html","./romeo_a_julie_004.html","./romeo_a_julie_006.html","./romeo_a_julie_005.html","./romeo_a_julie_008.html","./romeo_a_julie_007.html","./romeo_a_julie_009.html","./romeo_a_julie_010.html","./romeo_a_julie_011.html","./romeo_a_julie_012.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/LiterataTT-TextRegular.woff2","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image003.jpg","./resources/image001.jpg","./resources/image004.jpg","./resources/obalka_romeo_a_julie2.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./template-images/circles.png","./style/style.min.css"]));
+  return caches.open(CACHE).then(cache => cache.addAll(["./","./colophon.html","./favicon.png","./index.html","./manifest.json","./romeo_a_julie_002.html","./romeo_a_julie_005.html","./romeo_a_julie_006.html","./romeo_a_julie_007.html","./romeo_a_julie_008.html","./romeo_a_julie_009.html","./romeo_a_julie_010.html","./romeo_a_julie_011.html","./fonts/Literata-Italic-var.woff2","./fonts/Literata-var.woff2","./fonts/LiterataTT-TextItalic.woff2","./fonts/LiterataTT-TextRegular.woff2","./fonts/LiterataTT-TextSemibold.woff2","./fonts/LiterataTT_LICENSE.txt","./fonts/SpaceGroteskVF.woff2","./fonts/SpaceGroteskVF_LICENSE.txt","./resources/image001.jpg","./resources/image003.jpg","./resources/image004.jpg","./resources/obalka_romeo_a_julie2.jpg","./resources/upoutavka_eknihy.jpg","./scripts/bundle.js","./style/style.min.css","./template-images/circles.png"]));
 }
 
 self.addEventListener('fetch', e => {
